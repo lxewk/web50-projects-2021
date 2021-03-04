65844c747a7c43f096c7ef56dade0e75e3468b51
Elles <lxewk@me.com>
2021-03-04 11:29:25 +0100

    First commit Search project

---
+++ projects/2020/x/search/script.js
@@ -0,0 +1,21 @@
+document.getElementById("Lucky").addEventListener("click", function() {
+    console.log("script is reached!")
+      
+});
+
+
+ 
+// window.location.assign("https://www.google.com/search"); -> goes to the https , but the var lucky and var feeling aren't fetch.
+// var lucky = document.getElementsByClassName("yuRUbf")[0];
+// var feeling = lucky.getElementsByTagName("A")[0];
+
+// window.location.assign(feeling);
+
+// function myFunction() {
+//     var x = location.href;
+//     document.getElementById("demo").innerHTML = x;
+
+// document.getElementById("searchId").innerHTML = feeling;
+
+// document.getElementById("searchId").action = feeling;
+
